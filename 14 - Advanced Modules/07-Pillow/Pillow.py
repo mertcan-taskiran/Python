a51c45519c6b8c05c56ae12da0dba214cc03cb14
Mertcan Taşkıran <65121494+mertcan-taskiran@users.noreply.github.com>
2024-03-19 18:45:47 +0300

    pillow

---
+++ 14 - Advanced Modules/07-Pillow/Pillow.py
@@ -0,0 +1,15 @@
+from PIL import Image,ImageFilter
+
+image = Image.open("images/kus.jpg")
+
+# image.save("images/kus2.jpg") # Aynı resmi kaydeder.
+
+# image.rotate(180).save("images/kus3.jpg") # 180 derece döndürür.
+
+# image.convert(mode = "L").save("images/kus4.jpg") # Resmi siyah beyaz yapar.
+
+# degistir = (100,100)
+# image.thumbnail(degistir)
+# image.save("images/kus5.jpg") # Resim boyutunu değiştirir.
+
+# image.filter(ImageFilter.GaussianBlur(10)).save("images/kus6.jpg") # Blur
