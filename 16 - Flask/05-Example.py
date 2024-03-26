79e1329674660fb2a165b98c249b77a5657f4257
Mertcan Taşkıran <65121494+mertcan-taskiran@users.noreply.github.com>
2024-03-26 15:20:10 +0300

    flask bootstrap, koşul ve döngü kullanımı

--- 16 - Flask/05-Example.py
+++ 16 - Flask/05-Example.py
@@ -1,9 +1,21 @@
 from flask import Flask, render_template
 
 app = Flask(__name__)
+
 @app.route("/")
 def index():
-    return render_template("index.html")
+    # products = ["Ürün 1", "Ürün 2", "Ürün 3"]
+    products = [
+            {"id":1, "name":"Ürün 1", "price":1000},
+            {"id":2, "name":"Ürün 2", "price":2000},
+            {"id":3, "name":"Ürün 3", "price":3000},
+        ]
+
+    return render_template("index.html", stok=1, products = products) # Koşul Durumu ve Döngü
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
