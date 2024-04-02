0f5e9f0efff3f32a5817690c3c58a2921c2a97fd
Mertcan Taşkıran <65121494+mertcan-taskiran@users.noreply.github.com>
2024-04-02 22:47:03 +0300

    static files

--- Django/Django/settings.py
+++ Django/Django/settings.py
@@ -1,3 +1,4 @@
+import os
 """
 Django settings for Django project.
 
@@ -55,7 +56,7 @@ ROOT_URLCONF = "Django.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": ["templates"],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
@@ -111,6 +112,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = "static/"
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
--- Django/Django/urls.py
+++ Django/Django/urls.py
@@ -17,6 +17,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from product.views import index
+
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("", index, name="index"),
+    # path("index/", index),
 ]
