c938959626953e48402c5c8d5e0494b809c365d4
Mertcan Taşkıran <65121494+mertcan-taskiran@users.noreply.github.com>
2024-04-03 19:47:23 +0300

    register

--- Django/user/views.py
+++ Django/user/views.py
@@ -1,16 +1,33 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .forms import RegisterForm
+from django.contrib.auth.models import User
+from django.contrib.auth import login
 # Create your views here.
 
+# Register
 def register(request):
-    form = RegisterForm()
+    form = RegisterForm(request.POST or None)
+    if form.is_valid():
+        username = form.cleaned_data.get("username")
+        password = form.cleaned_data.get("password")
+
+        newUser = User(username =username)
+        newUser.set_password(password)
+
+        newUser.save()
+        login(request,newUser)
+
+        return redirect("index")
+    
     context = {
         "form" : form
     }
-    return render(request, "register.html", context)
 
-def login(request):
-    return render(request, "login.html")
+    return render(request,"register.html",context)
+
+    
+# def login(request):
+#     return render(request, "login.html")
 
 def logout(request):
     pass
