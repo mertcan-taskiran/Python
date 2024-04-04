0c0326087061038d8736548a2e454d1b40a55f05
Mertcan Taşkıran <65121494+mertcan-taskiran@users.noreply.github.com>
2024-04-04 15:16:47 +0300

    database ürün kaydetme

--- Django/product/models.py
+++ Django/product/models.py
@@ -5,8 +5,9 @@ from django.db import models
 class Product(models.Model):
     seller = models.ForeignKey("auth.User", on_delete = models.CASCADE, verbose_name = "Satıcı") # User silindiğinde ona ait olanlarda silinir.
     title = models.CharField(max_length = 50, verbose_name = "Ürün Adı")
+    # price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name="Fiyat")
     content = models.TextField(verbose_name = "İçerik")
     created_date = models.DateTimeField(auto_now_add = True, verbose_name = "Oluşturulma Tarihi") # Eklenme Tarihi
 
     def __str__(self):
-        return self.title # Object yerine 'Title' bilhisini göster.
+        return self.title # Object yerine 'Title' bilgisini göster.
--- Django/product/views.py
+++ Django/product/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, HttpResponse, redirect
 from .forms import ProductForm
-
+from django.contrib import messages
 # Create your views here.
 
 def index(request):
@@ -16,5 +16,14 @@ def dashboard(request):
     return render(request, "dashboard.html")
 
 def addProduct(request):
-    form = ProductForm()
+
+    form = ProductForm(request.POST or None)
+
+    if form.is_valid():
+        product = form.save(commit=False) # Kaydetme, sadece obje oluştur.
+        product.seller = request.user
+        product.save()
+        messages.success(request,"Başarıyla ürün eklendi.")
+        return redirect("index")
+
     return render(request, "addproduct.html", {"form":form})
