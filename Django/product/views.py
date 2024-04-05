c82a8914ea8509075f84013c68977f5f5276094e
Mertcan Taşkıran <65121494+mertcan-taskiran@users.noreply.github.com>
2024-04-05 13:59:44 +0300

    404 ekleme

--- Django/product/views.py
+++ Django/product/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, HttpResponse, redirect
+from django.shortcuts import render, HttpResponse, redirect, get_object_or_404
 from .forms import ProductForm
 from .models import Product
 from django.contrib import messages
@@ -37,5 +37,6 @@ def addProduct(request):
     return render(request, "addproduct.html", {"form":form})
 
 def detail(request, id):
-    product = Product.objects.filter(id=id).first()
+    # product = Product.objects.filter(id=id).first()
+    product = get_object_or_404(Product, id=id)
     return render(request, "detail.html", {"product": product})
